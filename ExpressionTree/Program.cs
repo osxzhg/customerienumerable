6846ea25604d78d5b37e2cd0d93ecc3a7b12cf94
osxzhg <osxzhg@gmail.com>
2020-12-24 23:36:36 +1300

    Using expresstree to do calculation

---
+++ ExpressionTree/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace ExpressionTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating parameter
+            var xParameter = Expression.Parameter(typeof(double), "x");
+            var yParameter = Expression.Parameter(typeof(double), "y");
+
+            //Creating the multiplication and addition expressions
+            var xSquared = Expression.Multiply(xParameter, xParameter);
+            var ySquared = Expression.Multiply(yParameter, yParameter);
+            var sum = Expression.Add(xSquared, ySquared);
+
+            // Creating a method call expressioin to call Math.Sqrt
+            var sqrtMethod = typeof(Math).GetMethod("Sqrt", new[] { typeof(double) });
+            var distance = Expression.Call(sqrtMethod, sum);
+
+            // Creating a Lambda expression
+            var distanceLambda = Expression.Lambda<Func<double,double,double>>(
+                distance,
+                xParameter,
+                yParameter);
+
+            Console.WriteLine($"The square root of x*x + y*y is {distanceLambda.Compile()(10, 5)}");
+
+
+        }
+    }
+}
